a207a298940d62d46b195b2f547e22aa19c16aac
Extrias <nitrofire2000@gmail.com>
2024-02-08 17:35:29 -0700

    lab0 done

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars(11x16) = c, Chars(5x7) = p, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -25,13 +25,18 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected 11x16 chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
     case 'a':
       puts("You selected arrow:");
       print_arrow(5, 7);
+      break;
+    case 'p':
+      puts("You selected 5x7 chars:");
+      for(char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
